8e5ad8d9f990fcb5dd8bfe4709a761199960964c
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2024-04-08 19:41:38 +0530

    Add files via upload
    hackerRank cpp solution for given problem statement.

---
+++ hackerRank_2.cpp
@@ -0,0 +1,13 @@
+/*Task
+Read  numbers from stdin and print their sum to stdout.
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n,p;
+    cin>>m>>n>>p;
+    int sum=m+n+p;
+    cout<<sum<<endl;
+    return 0;
+}
