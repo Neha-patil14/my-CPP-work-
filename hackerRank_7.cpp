1caed6fc0c977183a8827ed9c008d8f1797c12bb
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2024-04-23 21:55:38 +0530

    Add files via upload

---
+++ hackerRank_7.cpp
@@ -0,0 +1,20 @@
+//You will be given an array of  integers and you have to print the integers in the reverse order.
+//hackerRank problem statement and solution
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
